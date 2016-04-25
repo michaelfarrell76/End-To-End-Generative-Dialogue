19404c196cb0eba1782f91ed64163e65656287ae
Kevin Yang <kyang01@college.harvard.edu>
2016-04-25 04:01:54 -0400

    starting preprocessing

--- preprocess/src/generate.sh
+++
@@ -1,3 +0,0 @@
-python create_ubuntu_dataset.py "$@" --output 'train.csv' 'train'
-python create_ubuntu_dataset.py "$@" --output 'test.csv' 'test'
-python create_ubuntu_dataset.py "$@" --output 'valid.csv' 'valid'
