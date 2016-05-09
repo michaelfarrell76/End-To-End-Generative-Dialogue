b0d4f6c36d84bd817a0612266a313250bf1c3544
michaelfarrell76 <mkfrl09@gmail.com>
2016-05-09 01:44:30 -0400

    parallel stuff

--- src/parallel_functions.lua
+++ src/parallel_functions.lua
@@ -1,19 +1,14 @@
 -- Worker code
 function worker()
-    require "package"
-
     -- Alert successfully started up
     parallel.print('Im a worker, my ID is: ',  parallel.id, ' and my IP: ', parallel.ip)
-    parallel.print('parallel.parent ', parallel.parent)
 
     -- Global indicating is a child
     ischild = true
-
+    ext = ""
 
     -- Number of packages received
     local n_pkg = 0
-
-    ext = ""
 
     while true do
         -- Allow the parent to terminate the child
@@ -74,46 +69,6 @@ function worker()
     end
 end
 
-function setup_servers()
-    local p = require 'posix'
-
-    local instances = { ['104.154.22.185'] ='mikes-instance-group-4phn',   
-                        ['104.197.9.84'] = 'mikes-instance-group-8mir', 
-                        ['104.154.82.175'] = 'mikes-instance-group-9dze', 
-                        ['104.197.9.244'] = 'mikes-instance-group-de2i', 
-                        ['146.148.102.75'] = 'mikes-instance-group-m6dp', 
-                        ['104.197.179.152'] = 'mikes-instance-group-qwur', 
-                        ['104.197.244.249'] = 'mikes-instance-group-uh3b', 
-                        ['199.223.233.216'] = 'mikes-instance-group-usjf', 
-                        ['104.197.44.175'] = 'mikes-instance-group-vlvn' }
-
-    local pids = {}
-    for k, v in pairs(instances) do
-        local cpid = p.fork()
-        if cpid == 0 then -- child reads from pipe
-
-            setupEnvironment(v)
-            p._exit(0)
-        else -- parent writes to pipe
-            table.insert(pids, cpid)
-            -- wait for child to finish
-            
-        end
-        
-    end
-
-    for k, v in pairs(pids) do
-
-        p.wait(v)
-    end
-    
-
-    for k, v in pairs(instances) do
-        setupEnvironment(v)
-    end
-
-end
-
 -- The parent process function
 function parent()
     require "package"
@@ -136,12 +91,9 @@ function parent()
 
     if opt.remote then
         parallel.print('Runnign remotely')
-        -- Setup remote servers this isnt working i was playing around with the path variables and stuff but couldnt get it to connect
-        -- most likely either a problem with the google server not letting me in or im not setting up the lua environment correctly
-
-
-        package.path = "/home/michaelfarrell/.luarocks/share/lua/5.1/?.lua;/home/michaelfarrell/.luarocks/share/lua/5.1/?/init.lua;/home/michaelfarrell/torch/install/share/lua/5.1/?.lua;/home/michaelfarrell/torch/install/share/lua/5.1/?/init.lua;./?.lua;/home/michaelfarrell/torch/install/share/luajit-2.1.0-beta1/?.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua"
-        package.cpath = "/home/michaelfarrell/.luarocks/lib/lua/5.1/?.so;/home/michaelfarrell/torch/install/lib/lua/5.1/?.so;./?.so;/usr/local/lib/lua/5.1/?.so;/usr/local/lib/lua/5.1/loadall.so"
+        
+        -- package.path = "/home/michaelfarrell/.luarocks/share/lua/5.1/?.lua;/home/michaelfarrell/.luarocks/share/lua/5.1/?/init.lua;/home/michaelfarrell/torch/install/share/lua/5.1/?.lua;/home/michaelfarrell/torch/install/share/lua/5.1/?/init.lua;./?.lua;/home/michaelfarrell/torch/install/share/luajit-2.1.0-beta1/?.lua;/usr/local/share/lua/5.1/?.lua;/usr/local/share/lua/5.1/?/init.lua"
+        -- package.cpath = "/home/michaelfarrell/.luarocks/lib/lua/5.1/?.so;/home/michaelfarrell/torch/install/lib/lua/5.1/?.so;./?.so;/usr/local/lib/lua/5.1/?.so;/usr/local/lib/lua/5.1/loadall.so"
     
         
         fh,err = io.open("../client_list.txt")
