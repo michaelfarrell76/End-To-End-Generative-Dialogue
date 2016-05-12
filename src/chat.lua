4ecbf60144aca0ab8daf1fc13e44e71a65964834
michaelfarrell76 <mkfrl09@gmail.com>
2016-05-12 08:01:13 -0400

    old rando

--- src/chat.lua
+++ src/chat.lua
@@ -115,6 +115,8 @@ function chat(sbeam)
         local score_tensor = torch.DoubleTensor(scores)
 
         local pred = remove_pad(k_best[torch.multinomial(score_tensor, 1, true)[1]])
+                -- local pred = remove_pad(k_best[math.random(#k_best)])
+
         -- local pred = remove_pad(k_best[1])
 
         local pred_sent = wordidx2sent(pred, idx2word_targ, false)
