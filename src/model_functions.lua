c35eacc54f81a61f5799ac1baf139860b156d681
michaelfarrell76 <mkfrl09@gmail.com>
2016-05-13 22:52:10 -0400

    time

--- src/model_functions.lua
+++ src/model_functions.lua
@@ -468,10 +468,6 @@ function train(m, criterion, train_data, valid_data)
         local start_time = timer:time().real
         local num_words_target = 0
         local num_words_source = 0
-
-        if break_start ~= nil then
-            ignore_time = ignore_time + (timer:time().real - break_start)
-        end
         
         local i = 1
 
@@ -538,7 +534,7 @@ function train(m, criterion, train_data, valid_data)
                     stats = stats .. string.format('Training: %d/%d/%d total/source/target tokens/sec',
                         (num_words_target+num_words_source) / time_taken,
                         num_words_source / time_taken, num_words_target / time_taken)
-                    stats = stats .. string.format(' Time ellapse: %d', timer:time().real - ignore_time)
+                    stats = stats .. string.format(' Time ellapse: %d', timer:time().real)
                     opt.print(stats)
                 end
                 sys.sleep(.5)
@@ -587,7 +583,6 @@ function train(m, criterion, train_data, valid_data)
                 collectgarbage()
             end
         end
-        break_start = timer:time().real
         return train_loss, train_nonzeros
     end
 
@@ -626,8 +621,6 @@ function train(m, criterion, train_data, valid_data)
             end
         end
     end
-    break_start = nil
-    ignore_time = 0
 
     for epoch = opt.start_epoch, opt.num_epochs do
         -- Causing error after 1st epoch (likely because of clean_layer)
